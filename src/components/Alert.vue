9ea1de129c72a6f75e9fdb55ea945fbb7736a92d
silverprize <silverprizelee@gmail.com>
2019-12-31 15:27:51 +0900

    안내 메시지 출력용, Alert 컴포넌트 추가
    스타일은 나중에.

---
+++ src/components/Alert.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="alert">
+    {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Alert extends Vue {
+  @Prop({ type: String, required: true })
+  message!: string
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
