fc947cd6e2c64db02ef84004ba9d68f2cabd6803
silverprize <silverprizelee@gmail.com>
2020-01-01 14:42:17 +0900

    작업 안내표시용, 스피커 컴포넌트 추가

---
+++ src/components/Spinner.vue
@@ -0,0 +1,27 @@
+<template functional>
+  <div class="spinner" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Spinner extends Vue {
+}
+</script>
+
+<style lang="scss">
+@keyframes spinner {
+  to { transform: rotate(360deg); }
+}
+
+.spinner {
+  display: inline-block;
+  width: 2.5rem;
+  height: 2.5rem;
+  border: 0.3rem solid $yellow-color;
+  border-right-color: transparent;
+  border-radius: 50%;
+  animation: spinner .75s linear infinite;
+}
+</style>
