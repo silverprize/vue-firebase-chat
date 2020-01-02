2709a1e30eaaa2aad8929dc00c008c1c50a314b7
silverprize <silverprizelee@gmail.com>
2020-01-02 19:18:02 +0900

    VBadge 컴포넌트 완료

---
+++ src/components/VBadge/VBadge.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <span class="badge">
+    <slot />
+  </span>
+</template>
+
+<script lang="ts">
+import './VBadge.scss'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class VBadge extends Vue {}
+</script>
