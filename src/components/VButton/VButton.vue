5b8fdf21b565701775994b46310b8928a7e2143b
silverprize <silverprizelee@gmail.com>
2020-01-02 19:17:20 +0900

    VButton 컴포넌트 완료

---
+++ src/components/VButton/VButton.vue
@@ -0,0 +1,19 @@
+<template functional>
+  <button
+    class="button"
+    :class="[data.staticClass, data.class, `button_${props.variant || 'light'}`]"
+    :type="data.attrs && data.attrs.type || 'button'"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import './VButton.scss'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class VButton extends Vue {}
+</script>
