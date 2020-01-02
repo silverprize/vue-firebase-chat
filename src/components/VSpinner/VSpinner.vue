6c2ef57a424563696effb8b64d1e3379ca47388e
silverprize <silverprizelee@gmail.com>
2020-01-02 19:18:13 +0900

    VSpinner 컴포넌트 완료

---
+++ src/components/VSpinner/VSpinner.vue
@@ -0,0 +1,11 @@
+<template functional>
+  <div class="spinner" />
+</template>
+
+<script lang="ts">
+import './VSpinner.scss'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class VSpinner extends Vue {}
+</script>
