e839d19e6c5f00544335fe05dac9a719e5a37f5f
silverprize <silverprizelee@gmail.com>
2020-01-02 19:19:50 +0900

    eslint space rule 교정

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,10 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
-    'space-before-function-paren': 'off',
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'always', 'asyncArrow': 'always', named: 'never' },
+    ],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
