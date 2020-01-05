50c6350537b993c75c91cded35c5a14f895a0a42
silverprize <silverprizelee@gmail.com>
2020-01-05 17:22:57 +0900

    불필요, 각 페이지에서 담당함

--- src/router/chat-room/beforeEnter.ts
+++
@@ -1,8 +0,0 @@
-import { Location, Route } from 'vue-router'
-import store from '@/store'
-import { CLEAR } from '@/store/chat/mutations.type'
-
-export default async (to: Route, from: Route, next: (to?: Location) => void) => {
-  store.commit(CLEAR)
-  next()
-}
