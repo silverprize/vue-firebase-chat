57b62dff45c496f061886a670a37625403e7a683
silverprize <silverprizelee@gmail.com>
2020-01-06 23:24:14 +0900

    유틸기능 코드를 vue-test-utils에서 제공하는 기능으로 교체

--- tests/unit/VButton.spec.ts
+++ tests/unit/VButton.spec.ts
@@ -2,55 +2,32 @@ import { mount } from '@vue/test-utils'
 import VButton from '@/components/VButton/VButton.vue'
 
 describe('VButton.vue', () => {
-  it('레이블은 default slot으로 나타낸다.', () => {
+  it('레이블은 default slot으로 나타냄.', () => {
     const label = '레이블'
-    const wrapper = createVButton({ label })
-    console.info('text', wrapper.text())
+    const wrapper = mount(VButton, {
+      slots: { default: label },
+    })
     expect(wrapper.text()).toEqual(label)
   })
 
-  it('외부에서 받은 class attribute value가 나타나야 한다.', () => {
-    const externalClass = 'ext-class'
-    const wrapper = createVButton({ className: externalClass })
-    console.info('classes', wrapper.classes())
-    expect(wrapper.classes()).toContain(externalClass)
-  })
-
-  it('button type attribute와 동일하게 설정되어야 한다.', () => {
+  it('button type attribute와 유지.', () => {
     const type = 'submit'
-    const wrapper = createVButton({ type })
-    console.info('attributes', wrapper.attributes())
+    const wrapper = mount(VButton, {
+      propsData: { type },
+    })
     expect(wrapper.attributes().type).toEqual(type)
   })
 
-  it('type attribute 생략하면 type="button" 설정된다.', () => {
-    const wrapper = createVButton()
-    console.info('attributes', wrapper.attributes())
+  it('type attribute 생략하면 type="button" 설정.', () => {
+    const wrapper = mount(VButton)
     expect(wrapper.attributes().type).toEqual('button')
   })
 
-  it('버튼 스타일은 variant attribute value로 지정한다.', () => {
+  it('버튼 스타일은 variant attribute value로 지정.', () => {
     const variant = 'yellow'
-    const wrapper = createVButton({ variant })
-    console.info('classes', wrapper.classes())
+    const wrapper = mount(VButton, {
+      propsData: { variant },
+    })
     expect(wrapper.classes().find(c => c.includes(variant))).toBeTruthy()
   })
 })
-
-function createVButton(args:{ className?: string, type?: string, variant?:string, label?: string } = {}) {
-  const template = [
-    '<VButton',
-    args.className ? ` class="${args.className}"` : '',
-    args.type ? ` type="${args.type}"` : '',
-    args.variant ? ` variant="${args.variant}"` : '',
-    '>',
-    args.label ? args.label : '',
-    '</VButton>',
-  ].join('')
-  return mount({
-    components: {
-      VButton,
-    },
-    template,
-  }).find(VButton)
-}
