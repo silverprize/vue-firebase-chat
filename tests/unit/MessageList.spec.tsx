8a8f2079c5c54eb3b42d9a0e0320dd88d5be5176
silverprize <silverprizelee@gmail.com>
2020-03-03 00:19:15 +0900

    Test - MessageList.tsx 유닛 테스트
    - props, render 동작 검사

---
+++ tests/unit/MessageList.spec.tsx
@@ -0,0 +1,24 @@
+import MessageList from '@/components/MessageList/MessageList'
+import { mount } from '@vue/test-utils'
+import { Message } from '../../src/types'
+
+describe('MessageList', () => {
+  test('props', () => {
+    const wrapper = mount(MessageList, {
+      propsData: {
+        messageList: [
+          { sequence: 1, content: 'a' },
+          { sequence: 2, content: 'b' },
+        ],
+      },
+      scopedSlots: {
+        default({ message }: { message: Message }) {
+          return <span>{message.content}</span>
+        },
+      },
+    })
+
+    expect(wrapper.findAll('li').length).toBe(2)
+    expect(wrapper.text()).toBe('ab')
+  })
+})
